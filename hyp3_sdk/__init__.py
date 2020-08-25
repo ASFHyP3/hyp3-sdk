604feab84b5c7aa36bc1a2776b9e904b0b63ea33
Joseph H Kennedy <me@jhkennedy.org>
2020-08-25 11:19:37 -0800

    Add packaging structure

--- hyp3_sdk/__init__.py
+++ hyp3_sdk/__init__.py
@@ -1,7 +1,22 @@
+"""A python wrapper around the HyP3 API"""
+
+# FIXME: Python 3.8+ this should be `from importlib.metadata...`
+from importlib_metadata import PackageNotFoundError, version
+
 from .hyp3 import HyP3, HYP3_PROD, HYP3_TEST
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    print('package is not installed!\n'
+          'Install in editable/develop mode via (from the top of this repo):\n'
+          '   python -m pip install -e .\n'
+          'Or, to just get the version number use:\n'
+          '   python setup.py --version')
 
 __all__ = [
     'HyP3',
     'HYP3_PROD',
     'HYP3_TEST',
+    '__version__'
 ]
--- hyp3_sdk/hyp3.py
+++ hyp3_sdk/hyp3.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone
+from datetime import datetime
 from urllib.parse import urljoin
 
 import requests
