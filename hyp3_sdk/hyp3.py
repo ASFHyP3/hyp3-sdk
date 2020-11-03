60079463d2c06b13d1008dddf2dfd3ee76e629d4
Joseph H Kennedy <me@jhkennedy.org>
2020-11-03 11:55:38 -0900

    BROKEN: start sketch out refactor

--- hyp3_sdk/hyp3.py
+++ hyp3_sdk/hyp3.py
@@ -13,6 +13,7 @@ HYP3_TEST = 'https://hyp3-test-api.asf.alaska.edu'
 
 class HyP3:
     """A python wrapper around the HyP3 API"""
+
     def __init__(self, api_url: str = HYP3_PROD, authenticated_session: Optional[requests.Session] = None):
         """
 
@@ -25,8 +26,20 @@ class HyP3:
         if self.session is None:
             self.session = get_authenticated_session()
 
+    @staticmethod
+    def supported_job_types() -> tuple:
+        """
+        Returns:
+            The supported job types
+        """
+        return (
+            'RTC_GAMMA',
+            'INSAR_GAMMA',
+            'AUTORIFT',
+        )
+
     def get_jobs(self, start: Optional[datetime] = None, end: Optional[datetime] = None,
-                 status: Optional[str] = None, name: Optional[str] = None) -> dict:
+                 status: Optional[str] = None, name: Optional[str] = None) -> List[RequestedJob]:
         """Get your jobs
 
         Args:
@@ -51,28 +64,51 @@ class HyP3:
                 params['end'] += 'Z'
         if status is not None:
             params['status_code'] = status
-        return self.session.get(urljoin(self.url, '/jobs'), params=params).json()
 
-    def submit_jobs(self, jobs: List[Job], validate_only: bool = False) -> dict:
+        response = self.session.get(urljoin(self.url, '/jobs'), params=params).json()
+        jobs = [RequestedJobs.from_dict(job) for job in response['jobs']]
+        return jobs
+
+    def submit_jobs(self, jobs: List[Union[BaseJob, RequestedJob]], validate_only: bool = False,
+                    force_resubmit: bool = False) -> List[RequestedJob]:
         """Submit jobs to the API
 
         Args:
             jobs: A list of Job instances to submit to the API
             validate_only: Instead of submitting, just validate `jobs`
+            force_resubmit:
 
         Returns:
             The full dictionary representation of the HyP3 API response
         """
+        for job in jobs:
+            if isinstance(RequestedJob): # single disbatch submission?
+                if not job.is_expired() and not force_resubmit:
+                    raise ...
+
         payload = {
             'jobs': [job.to_dict() for job in jobs],
             'validate_only': validate_only,
         }
-        response = self.session.post(urljoin(self.url, '/jobs'), json=payload)
-        return response.json()
+        response = self.session.post(urljoin(self.url, '/jobs'), json=payload).json()
+        jobs = [RequestedJobs.from_dict(job) for job in response['jobs']]
+        return jobs
 
-    def get_quota(self) -> dict:
+    def jobs_are_complete(self, jobs: List[RequestedJob],
+                          wait: bool = False, timeout: int = 10800, check_every: int = 60) -> bool:
+
+
+
+    def my_info(self) -> dict:
         """
         Returns:
-            Your current quota
+            Your user information
         """
-        return self.session.get(urljoin(self.url, '/user')).json()['quota']
+        return self.session.get(urljoin(self.url, '/user')).json()
+
+    def check_quota(self) -> int:
+        """
+        Returns:
+            The number of jobs left in your quota
+        """
+        return self.session.get(urljoin(self.url, '/user')).json()['quota']["remaining"]
--- hyp3_sdk/jobs.py
+++ hyp3_sdk/jobs.py
@@ -1,15 +1,9 @@
 from hyp3_sdk.exceptions import ValidationError
 
-JOB_TYPES = [
-    'RTC_GAMMA',
-    'INSAR_GAMMA',
-    'AUTORIFT'
-]
 
-
-class Job:
+class BaseJob:
     """Jobs to be submitted to the API"""
-    def __init__(self, job_type: str, job_name: str, job_parameters: dict = {}):
+    def __init__(self, job_type: str, job_name: Optional[str] = None, job_parameters: dict = {}):
         """Create a new Job object
 
         Args:
@@ -17,7 +11,7 @@ class Job:
             job_name: A name for the job (must be <= 20 characters)
             job_parameters: Extra job parameters specifying custom processing options
         """
-        if job_type not in JOB_TYPES:
+        if job_type not in JOB_TYPES:  # FIXME: warning instead??
             raise ValidationError(f'Invalid job type: {job_type}, must be one of {JOB_TYPES}')
         if len(job_name) > 20:
             raise ValidationError('Job name too long, must be less then 20 chars')
@@ -38,59 +32,66 @@ class Job:
             'name': self.job_name,
         }
 
+    @staticmethod
+    def from_dict( ):
 
-def make_job(job_type: str, job_name: str, job_parameters: dict = {}) -> Job:
-    """Make a generic Job object
+    def submit(self):
 
-    Args:
-        job_type: The job type
-        job_name: A name for the job (must be <= 20 characters)
-        job_parameters: Extra job parameters specifying custom processing options
+class JobTemplate():
+    @staticmethod
+    def autorift(job_name: str, granule1: str, granule2: str, kwargs) -> BaseJob:
+        """Make an autoRIFT Job object
 
-    Returns:
-        A Job object
-    """
-    return Job(job_type, job_name, job_parameters)
+        Args:
+            job_name: A name for the job (must be <= 20 characters)
+            granule1: The first granule (scene) to use
+            granule2: The second granule (scene) to use
+
+        Returns:
+            A Job object
+        """
+        # TODO: warn about unsupported kwargs
+        return BaseJob(job_type='AUTORIFT', job_name=job_name, ...)
+
+    @staticmethod
+    def rtc(job_name: str, granule: str, ..., kwargs) -> BaseJob:
+        """Make an RTC Job object
+
+        Args:
+            job_name: A name for the job (must be <= 20 characters)
+            granule: The granule (scene) to process
+            extra_parameters: Extra job parameters specifying custom processing options
+
+        Returns:
+            A Job object
+        """
+        # TODO: warn about unsupported kwargs
+
+    @staticmethod
+    def insar(job_name: str, granule1: str, granule2: str, ..., kwargs) -> BaseJob:
+        """Make an InSAR Job object
+
+        Args:
+            job_name: A name for the job (must be <= 20 characters)
+            granule1: The first granule (scene) to use
+            granule2: The second granule (scene) to use
+            extra_parameters: Extra job parameters specifying custom processing options
+
+        Returns:
+            A Job object
+        """
+        # TODO: warn about unsupported kwargs
 
 
-def make_autorift_job(job_name: str, granule1: str, granule2: str) -> Job:
-    """Make an autoRIFT Job object
+class RequestedJob(BaseJob):
+    id =
+    product_files =
 
-    Args:
-        job_name: A name for the job (must be <= 20 characters)
-        granule1: The first granule (scene) to use
-        granule2: The second granule (scene) to use
+    def is_complete(self, wait: bool = False, timeout: int = 10800, check_every: int = 60) -> bool:
+        # FIXME: side affect update when complete?
 
-    Returns:
-        A Job object
-    """
-    return Job('AUTORIFT', job_name, {'granules': [granule1, granule2]})
+    def is_expired(self) -> bool:
 
+    def _update_instance_data(self, response: dict):
 
-def make_rtc_gamma_job(job_name: str, granule: str, extra_parameters: dict = {}) -> Job:
-    """Make an RTC Job object
-
-    Args:
-        job_name: A name for the job (must be <= 20 characters)
-        granule: The granule (scene) to process
-        extra_parameters: Extra job parameters specifying custom processing options
-
-    Returns:
-        A Job object
-    """
-    return Job('RTC_GAMMA', job_name, {'granules': [granule], **extra_parameters})
-
-
-def make_insar_gamma_job(job_name: str, granule1: str, granule2: str, extra_parameters: dict = {}) -> Job:
-    """Make an InSAR Job object
-
-    Args:
-        job_name: A name for the job (must be <= 20 characters)
-        granule1: The first granule (scene) to use
-        granule2: The second granule (scene) to use
-        extra_parameters: Extra job parameters specifying custom processing options
-
-    Returns:
-        A Job object
-    """
-    return Job('INSAR_GAMMA', job_name, {'granules': [granule1, granule2], **extra_parameters})
+    def download_files(self):
