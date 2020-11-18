fc4e7ed4aff0ac91118123532fa5dcf19bff2956
James Rine <jlrine2@alaska.edu>
2020-11-18 10:42:00 -0900

    support both strings and Paths in download functions

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import responses
 
 from hyp3_sdk.util import download_file
@@ -9,3 +11,9 @@ def test_download_file(tmp_path):
     result_path = download_file('https://foo.com/file', tmp_path / 'file')
     assert result_path == (tmp_path / 'file')
     assert result_path.read_text() == 'foobar'
+
+    responses.add(responses.GET, 'https://foo.com/file2', body='foobar2')
+    result_path = download_file('https://foo.com/file', str(tmp_path / 'file'))
+    assert result_path == (tmp_path / 'file')
+    assert result_path.read_text() == 'foobar'
+    assert isinstance(result_path, Path)
