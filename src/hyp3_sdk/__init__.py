6b4f1e64b5eefc6c08b718f009126d30826b0421
jacquelynsmale <34557291+jacquelynsmale@users.noreply.github.com>
2023-01-10 10:30:43 -0900

    remove try/except error message

--- src/hyp3_sdk/__init__.py
+++ src/hyp3_sdk/__init__.py
@@ -6,14 +6,8 @@ from .config import TESTING  # noqa
 from .hyp3 import HyP3, PROD_API, TEST_API
 from .jobs import Batch, Job
 
-try:
-    __version__ = version(__name__)
-except PackageNotFoundError:
-    print('package is not installed!\n'
-          'Install in editable/develop mode via (from the top of this repo):\n'
-          '   python -m pip install -e .\n'
-          'Or, to just get the version number use:\n'
-          '   python setup.py --version')
+
+__version__ = version(__name__)
 
 __all__ = [
     'Batch',
