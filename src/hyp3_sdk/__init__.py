32130b773e48f43cc7f339066a838cd31255beb7
jacquelynsmale <34557291+jacquelynsmale@users.noreply.github.com>
2023-01-10 10:39:33 -0900

    remove unncessary package

--- src/hyp3_sdk/__init__.py
+++ src/hyp3_sdk/__init__.py
@@ -1,6 +1,6 @@
 """A python wrapper around the HyP3 API"""
 
-from importlib.metadata import PackageNotFoundError, version
+from importlib.metadata import version
 
 from .config import TESTING  # noqa
 from .hyp3 import HyP3, PROD_API, TEST_API
