75291c595a01e5230d8f2a3a954cbcb38e92fcb2
Jake Herrmann <jtherrmann@alaska.edu>
2023-03-21 16:39:10 -0800

    Fix regexes

--- tests/test_util.py
+++ tests/test_util.py
@@ -20,7 +20,7 @@ def test_get_authenticated_session_eula():
     with pytest.raises(
             AuthenticationError,
             match=r'^Pre authorization required for this application, please authorize by visiting '
-                  'the resolution url: https://urs.earthdata.nasa.gov/approve_app\?client_id=foo$'
+                  'the resolution url: https://urs\.earthdata\.nasa\.gov/approve_app\?client_id=foo$'
     ):
         util.get_authenticated_session('user', 'pass')
 
@@ -36,7 +36,7 @@ def test_get_authenticated_session_study_area():
     with pytest.raises(
             AuthenticationError,
             match=r'^Please update your profile for application required attributes Study Area: '
-                  'https://urs.earthdata.nasa.gov/profile$'
+                  'https://urs\.earthdata\.nasa\.gov/profile$'
     ):
         util.get_authenticated_session('user', 'pass')
 
