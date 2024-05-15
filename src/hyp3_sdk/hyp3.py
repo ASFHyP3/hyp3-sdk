8d23ec6be6749b75f7026b6941d1490961fc1311
jacquelynsmale <jacquelyn.smale@gmail.com>
2024-05-15 13:38:41 -0800

    add in exceptions

--- src/hyp3_sdk/hyp3.py
+++ src/hyp3_sdk/hyp3.py
@@ -16,7 +16,6 @@ from hyp3_sdk.jobs import Batch, Job
 PROD_API = 'https://hyp3-api.asf.alaska.edu'
 TEST_API = 'https://hyp3-test-api.asf.alaska.edu'
 
-
 class HyP3:
     """A python wrapper around the HyP3 API.
 
@@ -49,10 +48,17 @@ class HyP3:
         self._check_application_status()
 
     def _check_application_status(self) -> None:
+        help_url = 'https://hyp3.asf.alaska.edu/request_access'
         info = self.my_info()
-        if info['application_status'] != 'APPROVED':
+        if info['application_status'] == 'NOT_STARTED':
             warnings.warn(f'User {info["user_id"]} has not yet applied for a monthly credit allotment.'
-                          ' Please visit https://hyp3.asf.alaska.edu/request_access to submit your application.')
+                          f' Please visit {help_url} to submit your application.')
+        if info['application_status'] == 'PENDING':
+            warnings.warn(f'User {info["user_id"]}\'s request for access is pending review  For more information, '
+                          f'visit {help_url}')
+        elif info['application_status'] != 'APPROVED':
+            warnings.warn(f'{info["user_id"]}\'s request for access has been rejected. For more information, '
+                          f'visit {help_url}')
 
     def find_jobs(self,
                   start: Optional[datetime] = None,
