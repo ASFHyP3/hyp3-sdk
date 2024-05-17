aa1fbb5a844178deccb920de764454b0b8d1558a
Jake Herrmann <jtherrmann@alaska.edu>
2024-05-17 09:21:28 -0800

    remove extra space

--- src/hyp3_sdk/hyp3.py
+++ src/hyp3_sdk/hyp3.py
@@ -55,7 +55,7 @@ class HyP3:
             warnings.warn(f'User {info["user_id"]} has not yet applied for a monthly credit allotment.'
                           f' Please visit {help_url} to submit your application.')
         if info['application_status'] == 'PENDING':
-            warnings.warn(f'User {info["user_id"]}\'s request for access is pending review.  For more information, '
+            warnings.warn(f'User {info["user_id"]}\'s request for access is pending review. For more information, '
                           f'visit {help_url}')
         if info['application_status'] == 'REJECTED':
             warnings.warn(f'{info["user_id"]}\'s request for access has been rejected. For more information, '
